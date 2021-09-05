5511bc466b4a37fd95381026ae0d4d3171d3645c
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-09-05 15:37:52 -0500

    Done.

---
+++ src/components/Messages.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Messages = () => {
+    return (
+        <div>
+            <form>
+                <h1>Message</h1>
+                <div>
+                    <textarea></textarea>
+                </div>
+                <div>
+                    <input type="submit" value="Send" />
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Messages
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import React from "react";
+import ReactDOM from "react-dom";
+/* Add NavLink to importer */
+import Home from './components/Home'
+import About from './components/About'
+import Messages from './components/Messages'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Navbar from './Navbar'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/messages" component={Messages} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+    </div>
+  </Router>,
+  document.getElementById("root")
 );
